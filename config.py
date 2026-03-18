5a4702c8ac77fdee5960d00c61197269774dfa9c
zevsket <gadzik403@gmail.com>
2026-03-18 20:26:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Конфигурация бота
 API_ID = int(os.environ.get("API_ID", 32510266))
-API_HASH = os.environ.get("API_HASH", "b65af61f1c3e54d29b5b555fd996e5cb") 
+API_HASH = os.environ.get("API_HASH", "b65af61f1c3e54d29b5b555fd996e5cb")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8611210220:AATtWNL75G5FVyHFHsb_5TWizbXTTGwFD6Q")
 
 # База данных
